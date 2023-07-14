d5db54be68181f97e4b9ff2aa476c3af19221eca
Sean Behan <codebam@riseup.net>
2023-07-14 16:03:12 -0400

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
 use worker::*;
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
 
 #[event(fetch)]
-async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+async fn main(req: Request, env: Env, ctx: worker::Context) -> Result<Response> {
+    Response::from_html(yew::ServerRenderer::<App>::new().render().await)
 }
